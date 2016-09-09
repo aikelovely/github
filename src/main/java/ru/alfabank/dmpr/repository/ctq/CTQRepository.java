764b04eed7d50d229da7fc140cb313445ce52708
U_M0U9C <abelkov@alfa.ru>
2016-09-09 13:51:49 +0300

    Изменения Александра.. 08092016 после мерджа2

--- src/main/java/ru/alfabank/dmpr/repository/ctq/CTQRepository.java
+++ src/main/java/ru/alfabank/dmpr/repository/ctq/CTQRepository.java
@@ -3,10 +3,7 @@ package ru.alfabank.dmpr.repository.ctq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.alfabank.dmpr.mapper.ctq.CTQMapper;
-import ru.alfabank.dmpr.model.ctq.CTQDashboardDynamicQueryOptions;
-import ru.alfabank.dmpr.model.ctq.CTQDashboardIndexItem;
-import ru.alfabank.dmpr.model.ctq.CTQDashboardOptions;
-import ru.alfabank.dmpr.model.ctq.CTQDashboardReportQueryOptions;
+import ru.alfabank.dmpr.model.ctq.*;
 
 /**
  * Репозиторий для графиков витрин Витрина показателей CTQ
@@ -39,5 +36,5 @@ public class CTQRepository {
      * Получает значения показателей для витрины Витрина показателей CTQ"
      * @return значения показателей
      */
-    public CTQDashboardIndexItem[] getReportDataSummary(CTQDashboardReportQueryOptions options){ return mapper.getReportDataSummary(options); }
+    public CTQDashboardSumData[] getReportDataSummary(){return mapper.getReportDataSummary();};
 }
