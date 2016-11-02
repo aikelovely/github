f2cc120af76058b2a6c6448697b75a71bb35f2c0
U_M0U9C <abelkov@alfa.ru>
2016-11-02 11:42:40 +0300

    баста

--- src/main/java/ru/alfabank/dmpr/model/unitCost/unitCost.java
+++ src/main/java/ru/alfabank/dmpr/model/unitCost/unitCost.java
@@ -3,7 +3,7 @@ package ru.alfabank.dmpr.model.unitCost;
 import org.joda.time.LocalDate;
 
 /**
- * Набор данных для формирования выгрузки в Excel. 12
+ * Набор данных для формирования выгрузки в Excel. 123
  */
 public class unitCost {
     public String prdtypeCcode;
