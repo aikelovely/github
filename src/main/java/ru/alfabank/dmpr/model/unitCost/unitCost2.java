e98767db6671fd7a316c19f377afeb6de2b03690
aike <aikelovely@mail.ru>
2018-01-08 01:16:05 +0300

    Женя к Айке

---
+++ src/main/java/ru/alfabank/dmpr/model/unitCost/unitCost2.java
@@ -0,0 +1,23 @@
+package ru.alfabank.dmpr.model.unitCost;
+
+import org.joda.time.LocalDate;
+
+/** 2
+ * Набор данных для формирования выгрузки в Excel.4
+ */
+public class UnitCost2 {
+    public String prdtypeCcode;
+    public String lvl;
+    public LocalDate valueDay;
+    public String bpinnerendproductName;
+    public String bpinnerendproductCcode;
+    public String bporgregionCcode;
+    public String bpdivisiongroupIepCcode;
+    public Double totalbpiepquantityCnt;
+    public Double totalBpiepQuantityCnt;
+    public Double totallaborvalueCnt;
+    public Double totalcostusdCnt;
+    public Double totalcostrurCnt;
+    public Double ucUsd;
+    public Double ucRur;
+}
